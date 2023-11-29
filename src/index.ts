41852ffa140d35982a404d2b2d103998e7ffcaae
OKTAY ŞAHİN <osahin@akimmetal.com.tr>
2023-11-29 15:28:10 +0300

    bot token changed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 async function getTelegramData() {
-  const response = await fetch(
-    "https://api.telegram.org/bot5917573567:AAFvAdaSfPN8VldpyhJoihVfwS4WKzwEsPo/getMe"
-  );
+  const botToken = "6012991797:AAGK-9Jzb_WdpBWD92F2b62BlqQEnm5FifE"; // my telegram test bot token
+
+  const response = await fetch(`https://api.telegram.org/bot${botToken}/getMe`);
   const data = await response.json();
 
   return data;
